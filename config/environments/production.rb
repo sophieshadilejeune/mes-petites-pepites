d0247687ca8b542394c104ca5b7bf9048da1010e
Sophie Lejeune <sophie.shadi.lejeune@gmail.com>
2020-06-09 19:23:10 +1000

    added email address for reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,14 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
+  config.action_mailer.default_url_options = { host: "https://mes-petites-pepites.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
